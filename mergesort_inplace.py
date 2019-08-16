21d195d490815266c2de81c3b8bedda2ccf6ac84
Rinze Watanabe <rinzewatanabe@d-i89-204-245.student.eduroam.uq.edu.au>
2019-08-16 15:34:24 +1000

    Implemented mergesort inplace

---
+++ mergesort_inplace.py
@@ -0,0 +1,28 @@
+def mergesort_inplace(target):
+
+    unit = 1
+
+    while unit <= len(target):
+        h = 0
+        for h in range(0, len(target), unit * 2):
+            l = h
+            r = min(len(target), h + 2 * unit)
+            mid = h + unit
+            p = l
+            q = mid
+
+            while p < mid and q < r:
+                if target[p] < target[q]:
+                    p += 1
+                else:
+                    tmp = target[q]
+                    target[p + 1: q + 1] = target[p:q]
+                    target[p] = tmp
+                    p += 1
+                    mid += 1
+                    q += 1
+
+        unit *= 2
+    
+    return target
+    
