93f23433bff43670c73656b4e7776d28f896106e
RW <rinze.watanabe.translation@gmail.com>
2019-08-21 21:25:08 +1000

    Implemented __str__ dunder for linked list

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -5,12 +5,24 @@ class Node:
     def __init__(self, val):
         self.val = val
         self.next = None
-    
+
+    def __str__(self):
+        temp = self
+        s = ["head: ", temp.val]
+        while temp.next is not None:
+            temp = self.next
+            s.append(" -> ")
+            s.append(temp.val)
+
+        return "".join(s)
+
     def link(self, node: Node):
         self.next = node
 
-# example 
+
+# example
 node_1 = Node('a')
 node_2 = Node('b')
 
 node_1.link(node_2)
+print(node_1)
