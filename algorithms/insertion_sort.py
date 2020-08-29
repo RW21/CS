8a1c073cb57ed385f290f2132889418543fbdfbe
RW21 <rinze.watanabe.translation@gmail.com>
2020-08-29 23:12:58 +1000

    Update (bu)

--- algorithms/insertion_sort.py
+++ algorithms/insertion_sort.py
@@ -1,5 +1,5 @@
 def insertion_sort(target: list):
-    for i, _ in enumerate(target):
+    for i in range(len(target)):
         j = i
         while j > 0 and target[j-1] > target[j]:
             target[j], target[j-1] = target[j-1], target[j]
